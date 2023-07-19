c4d74ffaf22b1720238ec6444ca378f4309d3c2c
Zaf <109876909+ZafirProjects@users.noreply.github.com>
2023-07-19 19:16:20 +0100

    Created simple form with label

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import "./styles.css"
+
 export default function App() {
-  return "Yo"
+  return <form className="">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" id="item" />
+    </div>
+  </form>
 }
